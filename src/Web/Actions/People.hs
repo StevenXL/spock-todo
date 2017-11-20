009af53d0017273ee83d713fd44744b84fe21918
Steven Leiva <leiva.steven@gmail.com>
2017-11-19 23:35:30 -0500

    Testing

--- src/Web/Actions/People.hs
+++ src/Web/Actions/People.hs
@@ -7,9 +7,11 @@
 
 module Web.Actions.People where
 
+import Control.Monad ((>=>))
 import Data.Aeson
        (FromJSON, ToJSON, Value(String), (.=), object, toJSON)
 import Data.Maybe (fromMaybe)
+import Data.String (IsString)
 import Data.Text (Text, null, unpack)
 import Database.Persist
        (Entity, SelectOpt(Asc), delete, get, getBy, insert, replace,
@@ -18,11 +20,13 @@ import GHC.Generics (Generic)
 import Model.CoreTypes
 import Network.HTTP.Types
        (status201, status400, status404, status406)
+import Prelude hiding (even)
+import qualified Prelude
 import Text.Digestive
        (Form, Result(Error, Success), (.:), check, text, validate,
         validateM)
 import Text.Digestive.Aeson (digestJSON, jsonErrors)
-import Text.Digestive.Util (readMaybe)
+import Text.Read (readMaybe)
 import Web.Configuration.Database (runSQL)
 import Web.Configuration.ErrorCode (mkErrorCode, unknownError)
 import Web.Configuration.Response (ApiAction, errorJson)
@@ -104,18 +108,15 @@ instance ToJSON RegistrationRequest
 registerForm :: Form Text ApiAction RegistrationRequest
 registerForm =
     pure RegistrationRequest <*> "name" .: validateName <*>
-    "age" .: validate validateAge (text Nothing) <*>
+    "age" .:
+    validate
+        (notEmpty >=> integer >=> even >=> greaterThan 0 >=> lessThanOrEq 100)
+        (text Nothing) <*>
     "email" .: validateEmail
   where
     nonEmptyText = check "Cannot be empty" (not . Data.Text.null) $ text Nothing
     validateName = nonEmptyText
     validateEmail = validateM getPersonByEmail nonEmptyText
-    validateAge :: Text -> Result Text Int
-    validateAge potentialInt =
-        maybe
-            (Error "Unable to parse age")
-            Success
-            (readMaybe (unpack potentialInt) :: Maybe Int)
 
 getPersonByEmail :: Text -> ApiAction (Result Text Text)
 getPersonByEmail email' = do
@@ -124,3 +125,31 @@ getPersonByEmail email' = do
     case mPerson of
         Nothing -> return $ Success "Email is available"
         Just _ -> return $ Error "Email is not available"
+
+notEmpty :: IsString v => Text -> Result v Text
+notEmpty input =
+    if (not $ Data.Text.null input)
+        then Success "Valid Integer"
+        else Error "Cannot be empty"
+
+integer :: Text -> Result Text Int
+integer input =
+    maybe (Error "Unable to parse int") Success (readMaybe $ unpack input)
+
+greaterThan :: (Num a, Ord a, Show a) => a -> a -> Result Text a
+greaterThan base other =
+    if other > base
+        then Success other
+        else Error "Not big enough"
+
+lessThanOrEq :: (Num a, Ord a, Show a) => a -> a -> Result Text a
+lessThanOrEq base other =
+    if other <= base
+        then Success other
+        else Error "Not small or equal enough"
+
+even :: Int -> Result Text Int
+even int =
+    if (Prelude.even int)
+        then Success int
+        else Error "Not even"
