fd26315c8876f26c132f8cb90cf01bb4f7a9eb1a
Steven Leiva <leiva.steven@gmail.com>
2017-11-21 18:16:03 -0500

    Move RegistrationForm into the Forms directory.

--- src/Web/Actions/People.hs
+++ src/Web/Actions/People.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -7,29 +6,19 @@
 
 module Web.Actions.People where
 
-import Control.Monad ((>=>))
-import Data.Aeson
-       (FromJSON, ToJSON, Value(String), (.=), object, toJSON)
+import Data.Aeson (Value(String), (.=), object, toJSON)
 import Data.Maybe (fromMaybe)
-import Data.String (IsString)
-import Data.Text (Text, null, unpack)
 import Database.Persist
-       (Entity, SelectOpt(Asc), delete, get, getBy, insert, replace,
-        selectList)
-import GHC.Generics (Generic)
+       (Entity, SelectOpt(Asc), delete, get, insert, replace, selectList)
 import Model.CoreTypes
 import Network.HTTP.Types
        (status201, status400, status404, status406)
-import Prelude hiding (even)
-import qualified Prelude
-import Text.Digestive
-       (Form, Result(Error, Success), (.:), check, text, validate,
-        validateM)
 import Text.Digestive.Aeson (digestJSON, jsonErrors)
-import Text.Read (readMaybe)
 import Web.Configuration.Database (runSQL)
 import Web.Configuration.ErrorCode (mkErrorCode, unknownError)
 import Web.Configuration.Response (ApiAction, errorJson)
+import Web.Forms.RegistrationForm
+       (RegistrationRequest(..), registerForm)
 import Web.Spock (json, jsonBody, setStatus)
 
 index :: ApiAction ()
@@ -94,66 +83,3 @@ create = do
                     personId <- runSQL $ insert (Person name age email)
                     json $
                         object ["result" .= String "success", "id" .= personId]
-
-data RegistrationRequest = RegistrationRequest
-    { name :: Text
-    , age :: Int
-    , email :: Text
-    } deriving (Generic)
-
-instance FromJSON RegistrationRequest
-
-instance ToJSON RegistrationRequest
-
-registerForm :: Form Text ApiAction RegistrationRequest
-registerForm =
-    pure RegistrationRequest <*> "name" .: validateName <*>
-    "age" .:
-    validate
-        (notEmpty >=> integer >=> even >=> greaterThan 0 >=> lessThanOrEq 100)
-        (text Nothing) <*>
-    "email" .: validateEmail
-  where
-    nonEmptyText = check "Cannot be empty" (not . Data.Text.null) $ text Nothing
-    validateName = nonEmptyText
-    validateEmail = validateM getPersonByEmail nonEmptyText
-
-getPersonByEmail :: Text -> ApiAction (Result Text Text)
-getPersonByEmail email' = do
-    mPerson :: Maybe (Entity Person) <-
-        runSQL $ getBy (UniquePersonEmail email')
-    case mPerson of
-        Nothing -> return $ Success email'
-        Just _ -> return $ Error "Email is not available"
-
-notEmpty :: IsString v => Text -> Result v Text
-notEmpty input =
-    if (not $ Data.Text.null input)
-        then Success input
-        else Error "Cannot be empty"
-
-integer :: Text -> Result Text Int
-integer input =
-    maybe (Error "Unable to parse int") Success (textToMaybeInt input)
-  where
-    textToMaybeInt :: Text -> Maybe Int
-    textToMaybeInt text' =
-        fmap round (readMaybe . unpack $ text' :: Maybe Double)
-
-greaterThan :: (Num a, Ord a, Show a) => a -> a -> Result Text a
-greaterThan base other =
-    if other > base
-        then Success other
-        else Error "Not big enough"
-
-lessThanOrEq :: (Num a, Ord a, Show a) => a -> a -> Result Text a
-lessThanOrEq base other =
-    if other <= base
-        then Success other
-        else Error "Not small or equal enough"
-
-even :: Int -> Result Text Int
-even int =
-    if (Prelude.even int)
-        then Success int
-        else Error "Not even"
---
+++ src/Web/Forms/RegistrationForm.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Web.Forms.RegistrationForm where
+
+import Control.Monad ((>=>))
+import Data.Aeson (FromJSON, ToJSON)
+import Data.String (IsString)
+import Data.Text (Text, null, unpack)
+import Database.Persist (Entity, getBy)
+import GHC.Generics (Generic)
+import Model.CoreTypes (Person, Unique(UniquePersonEmail))
+import Text.Digestive
+       (Form, Result(Error, Success), (.:), check, text, validate,
+        validateM)
+import Text.Read (readMaybe)
+import Web.Configuration.Database (runSQL)
+import Web.Configuration.Response (ApiAction)
+
+data RegistrationRequest = RegistrationRequest
+    { name :: Text
+    , age :: Int
+    , email :: Text
+    } deriving (Generic)
+
+instance FromJSON RegistrationRequest
+
+instance ToJSON RegistrationRequest
+
+registerForm :: Form Text ApiAction RegistrationRequest
+registerForm =
+    pure RegistrationRequest <*> "name" .: validateName <*>
+    "age" .:
+    validate
+        (notEmpty >=> integer >=> greaterThan 0 >=> lessThanOrEq 100)
+        (text Nothing) <*>
+    "email" .: validateEmail
+  where
+    nonEmptyText = check "Cannot be empty" (not . Data.Text.null) $ text Nothing
+    validateName = nonEmptyText
+    validateEmail = validateM getPersonByEmail nonEmptyText
+
+getPersonByEmail :: Text -> ApiAction (Result Text Text)
+getPersonByEmail email' = do
+    mPerson :: Maybe (Entity Person) <-
+        runSQL $ getBy (UniquePersonEmail email')
+    case mPerson of
+        Nothing -> return $ Success email'
+        Just _ -> return $ Error "Email is not available"
+
+notEmpty :: IsString v => Text -> Result v Text
+notEmpty input =
+    if (not $ Data.Text.null input)
+        then Success input
+        else Error "Cannot be empty"
+
+integer :: Text -> Result Text Int
+integer input =
+    maybe (Error "Unable to parse int") Success (textToMaybeInt input)
+  where
+    textToMaybeInt :: Text -> Maybe Int
+    textToMaybeInt text' =
+        fmap round (readMaybe . unpack $ text' :: Maybe Double)
+
+greaterThan :: (Num a, Ord a, Show a) => a -> a -> Result Text a
+greaterThan base other =
+    if other > base
+        then Success other
+        else Error "Not big enough"
+
+lessThanOrEq :: (Num a, Ord a, Show a) => a -> a -> Result Text a
+lessThanOrEq base other =
+    if other <= base
+        then Success other
+        else Error "Not small or equal enough"
